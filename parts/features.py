72d1c58137db8016bce514059addaec13726706b
YiLun <w86763777@gmail.com>
2020-06-09 15:54:39 +0800

    pep8

--- parts/features.py
+++ parts/features.py
@@ -18,31 +18,38 @@ import math
 import librosa
 from .perturb import AudioAugmentor
 from .segment import AudioSegment
-from apex import amp
 
 
-def audio_from_file(file_path, offset=0, duration=0, trim=False, target_sr=16000):
+def audio_from_file(file_path, offset=0, duration=0, trim=False,
+                    target_sr=16000):
     audio = AudioSegment.from_file(file_path,
                                    target_sr=target_sr,
                                    int_values=False,
                                    offset=offset, duration=duration, trim=trim)
-    samples=torch.tensor(audio.samples, dtype=torch.float).cuda()
+    samples = torch.tensor(audio.samples, dtype=torch.float).cuda()
     num_samples = torch.tensor(samples.shape[0]).int().cuda()
     return (samples.unsqueeze(0), num_samples.unsqueeze(0))
 
+
 class WaveformFeaturizer(object):
     def __init__(self, input_cfg, augmentor=None):
-        self.augmentor = augmentor if augmentor is not None else AudioAugmentor()
+        if augmentor is not None:
+            self.augmentor = augmentor
+        else:
+            self.augmentor = AudioAugmentor()
         self.cfg = input_cfg
 
     def max_augmentation_length(self, length):
         return self.augmentor.max_augmentation_length(length)
 
     def process(self, file_path, offset=0, duration=0, trim=False):
-        audio = AudioSegment.from_file(file_path,
-                                       target_sr=self.cfg['sample_rate'],
-                                       int_values=self.cfg.get('int_values', False),
-                                       offset=offset, duration=duration, trim=trim)
+        audio = AudioSegment.from_file(
+            file_path,
+            target_sr=self.cfg['sample_rate'],
+            int_values=self.cfg.get('int_values', False),
+            offset=offset,
+            duration=duration,
+            trim=trim)
         return self.process_segment(audio)
 
     def process_segment(self, audio_segment):
@@ -61,8 +68,10 @@ class WaveformFeaturizer(object):
 
 # @torch.jit.script
 # def normalize_batch_per_feature(x, seq_len):
-#     x_mean = torch.zeros((seq_len.shape[0], x.shape[1]), dtype=x.dtype, device=x.device)
-#     x_std = torch.zeros((seq_len.shape[0], x.shape[1]), dtype=x.dtype, device=x.device)
+#     x_mean = torch.zeros(
+#         (seq_len.shape[0], x.shape[1]), dtype=x.dtype, device=x.device)
+#     x_std = torch.zeros(
+#         (seq_len.shape[0], x.shape[1]), dtype=x.dtype, device=x.device)
 
 #     for i in range(x.shape[0]):
 #         x_mean[i, :] = x[i, :, :seq_len[i]].mean(dim=1)
@@ -82,14 +91,16 @@ class WaveformFeaturizer(object):
 #     x_std += 1e-5
 #     return (x - x_mean.view(-1, 1, 1)) / x_std.view(-1, 1, 1)
 
+
 @torch.jit.script
 def normalize_batch(x, seq_len, normalize_type: str):
-#    print ("normalize_batch: x, seq_len, shapes: ", x.shape, seq_len, seq_len.shape)
+    # print("normalize_batch: x, seq_len, shapes: ",
+    #       x.shape, seq_len, seq_len.shape)
     if normalize_type == "per_feature":
         x_mean = torch.zeros((seq_len.shape[0], x.shape[1]), dtype=x.dtype,
-                                                 device=x.device)
+                             device=x.device)
         x_std = torch.zeros((seq_len.shape[0], x.shape[1]), dtype=x.dtype,
-                                                device=x.device)
+                            device=x.device)
         for i in range(x.shape[0]):
             x_mean[i, :] = x[i, :, :seq_len[i]].mean(dim=1)
             x_std[i, :] = x[i, :, :seq_len[i]].std(dim=1)
@@ -108,6 +119,7 @@ def normalize_batch(x, seq_len, normalize_type: str):
     else:
         return x
 
+
 @torch.jit.script
 def splice_frames(x, frame_splicing: int):
     """ Stacks frames together across feature dim
@@ -122,15 +134,19 @@ def splice_frames(x, frame_splicing: int):
         seq.append(torch.cat([x[:, :, :n + 1], x[:, :, n + 1:]], dim=2))
     return torch.cat(seq, dim=1)
 
+
 class SpectrogramFeatures(nn.Module):
-    # For JIT. See https://pytorch.org/docs/stable/jit.html#python-defined-constants
-    __constants__ = ["dither", "preemph", "n_fft", "hop_length", "win_length", "log", "frame_splicing", "window", "normalize", "pad_to", "max_duration", "do_normalize"]
+    # For JIT. See
+    # https://pytorch.org/docs/stable/jit.html#python-defined-constants
+    __constants__ = ["dither", "preemph", "n_fft", "hop_length", "win_length",
+                     "log", "frame_splicing", "window", "normalize", "pad_to",
+                     "max_duration", "do_normalize"]
 
     def __init__(self, sample_rate=8000, window_size=0.02, window_stride=0.01,
-                       n_fft=None,
-                       window="hamming", normalize="per_feature", log=True, 
-                       dither=1e-5, pad_to=8, max_duration=16.7,
-                       frame_splicing=1):
+                 n_fft=None,
+                 window="hamming", normalize="per_feature", log=True,
+                 dither=1e-5, pad_to=8, max_duration=16.7,
+                 frame_splicing=1):
         super(SpectrogramFeatures, self).__init__()
         torch_windows = {
             'hann': torch.hann_window,
@@ -175,9 +191,10 @@ class SpectrogramFeatures(nn.Module):
             x += self.dither * torch.randn_like(x)
 
         # do preemphasis
-        if hasattr(self,'preemph') and self.preemph is not None:
-            x = torch.cat((x[:, 0].unsqueeze(1), x[:, 1:] - self.preemph * x[:, :-1]),
-                                        dim=1)
+        if hasattr(self, 'preemph') and self.preemph is not None:
+            x = torch.cat(
+                [x[:, 0].unsqueeze(1), x[:, 1:] - self.preemph * x[:, :-1]],
+                dim=1)
 
         # get spectrogram
         x = torch.stft(x, n_fft=self.n_fft, hop_length=self.hop_length,
@@ -196,16 +213,18 @@ class SpectrogramFeatures(nn.Module):
         # normalize if required
         x = normalize_batch(x, seq_len, normalize_type=self.normalize)
 
-        # mask to zero any values beyond seq_len in batch, pad to multiple of `pad_to` (for efficiency)
+        # mask to zero any values beyond seq_len in batch, pad to multiple of
+        # `pad_to` (for efficiency)
         max_len = x.size(-1)
-        mask = torch.arange(max_len, dtype=seq_len.dtype).to(seq_len.device).expand(x.size(0), max_len) >= seq_len.unsqueeze(1)
+        mask = torch.arange(max_len, dtype=seq_len.dtype).to(
+            seq_len.device).expand(x.size(0), max_len) >= seq_len.unsqueeze(1)
         x = x.masked_fill(mask.unsqueeze(1).to(device=x.device), 0)
-        
+
         # TORCHSCRIPT: Is this del important? It breaks scripting
-        #del mask
-    
+        # del mask
+
         pad_to = self.pad_to
-    
+
         # TORCHSCRIPT: Cant have mixed types. Using pad_to < 0 for "max"
         if pad_to < 0:
             x = nn.functional.pad(x, (0, self.max_length - x.size(-1)))
@@ -217,25 +236,33 @@ class SpectrogramFeatures(nn.Module):
 
     @classmethod
     def from_config(cls, cfg, log=False):
-        return cls(sample_rate=cfg['sample_rate'], window_size=cfg['window_size'],
+        return cls(sample_rate=cfg['sample_rate'],
+                   window_size=cfg['window_size'],
                    window_stride=cfg['window_stride'],
                    n_fft=cfg['n_fft'], window=cfg['window'],
                    normalize=cfg['normalize'],
                    max_duration=cfg.get('max_duration', 16.7),
                    dither=cfg.get('dither', 1e-5), pad_to=cfg.get("pad_to", 0),
                    frame_splicing=cfg.get("frame_splicing", 1), log=log)
-constant=1e-5
+
+
+constant = 1e-5
+
+
 class FilterbankFeatures(nn.Module):
-    # For JIT. See https://pytorch.org/docs/stable/jit.html#python-defined-constants
-    __constants__ = ["dither", "preemph", "n_fft", "hop_length", "win_length", "center", "log", "frame_splicing", "window", "normalize", "pad_to", "max_duration", "max_length"]
+    # For JIT. See
+    # https://pytorch.org/docs/stable/jit.html#python-defined-constants
+    __constants__ = ["dither", "preemph", "n_fft", "hop_length", "win_length",
+                     "center", "log", "frame_splicing", "window", "normalize",
+                     "pad_to", "max_duration", "max_length"]
 
     def __init__(self, sample_rate=8000, window_size=0.02, window_stride=0.01,
-                       window="hamming", normalize="per_feature", n_fft=None,
-                       preemph=0.97,
-                       nfilt=64, lowfreq=0, highfreq=None, log=True, dither=constant,
-                       pad_to=8,
-                       max_duration=16.7,
-                       frame_splicing=1):
+                 window="hamming", normalize="per_feature", n_fft=None,
+                 preemph=0.97,
+                 nfilt=64, lowfreq=0, highfreq=None, log=True, dither=constant,
+                 pad_to=8,
+                 max_duration=16.7,
+                 frame_splicing=1):
         super(FilterbankFeatures, self).__init__()
 
         torch_windows = {
@@ -246,13 +273,13 @@ class FilterbankFeatures(nn.Module):
             'none': None,
         }
 
-        self.win_length = int(sample_rate * window_size) # frame size
+        self.win_length = int(sample_rate * window_size)  # frame size
         self.hop_length = int(sample_rate * window_stride)
         self.n_fft = n_fft or 2 ** math.ceil(math.log2(self.win_length))
 
         self.normalize = normalize
         self.log = log
-        #TORCHSCRIPT: Check whether or not we need this
+        # TORCHSCRIPT: Check whether or not we need this
         self.dither = dither
         self.frame_splicing = frame_splicing
         self.nfilt = nfilt
@@ -263,8 +290,10 @@ class FilterbankFeatures(nn.Module):
         window_tensor = window_fn(self.win_length,
                                   periodic=False) if window_fn else None
         filterbanks = torch.tensor(
-            librosa.filters.mel(sample_rate, self.n_fft, n_mels=nfilt, fmin=lowfreq,
-                                                    fmax=highfreq), dtype=torch.float).unsqueeze(0)
+            librosa.filters.mel(
+                sample_rate, self.n_fft, n_mels=nfilt,
+                fmin=lowfreq, fmax=highfreq),
+            dtype=torch.float).unsqueeze(0)
         # self.fb = filterbanks
         # self.window = window_tensor
         self.register_buffer("fb", filterbanks)
@@ -282,27 +311,29 @@ class FilterbankFeatures(nn.Module):
 
     # do stft
     # TORCHSCRIPT: center removed due to bug
-    def  stft(self, x):
+    def stft(self, x):
         return torch.stft(x, n_fft=self.n_fft, hop_length=self.hop_length,
                           win_length=self.win_length,
                           window=self.window.to(dtype=torch.float))
+
     def forward(self, x, seq_len):
-        dtype = x.dtype
+        # dtype = x.dtype
 
         seq_len = self.get_seq_len(seq_len)
-        
+
         # dither
         if self.dither > 0:
             x += self.dither * torch.randn_like(x)
 
         # do preemphasis
         if self.preemph is not None:
-            x = torch.cat((x[:, 0].unsqueeze(1), x[:, 1:] - self.preemph * x[:, :-1]),
-                                        dim=1)
-            
-        x  = self.stft(x)
-            
-            # get power spectrum
+            x = torch.cat(
+                [x[:, 0].unsqueeze(1), x[:, 1:] - self.preemph * x[:, :-1]],
+                dim=1)
+
+        x = self.stft(x)
+
+        # get power spectrum
         x = x.pow(2).sum(-1)
 
         # dot with filterbank energies
@@ -319,10 +350,11 @@ class FilterbankFeatures(nn.Module):
         # normalize if required
         x = normalize_batch(x, seq_len, normalize_type=self.normalize)
 
-        # mask to zero any values beyond seq_len in batch, pad to multiple of `pad_to` (for efficiency)
+        # mask to zero any values beyond seq_len in batch, pad to multiple of
+        # `pad_to` (for efficiency)
         max_len = x.size(-1)
-        mask = torch.arange(max_len, dtype=seq_len.dtype).to(x.device).expand(x.size(0),
-                                                                              max_len) >= seq_len.unsqueeze(1)
+        mask = torch.arange(max_len, dtype=seq_len.dtype).to(x.device).expand(
+            x.size(0), max_len) >= seq_len.unsqueeze(1)
 
         x = x.masked_fill(mask.unsqueeze(1), 0)
         # TORCHSCRIPT: Is this del important? It breaks scripting
@@ -334,18 +366,20 @@ class FilterbankFeatures(nn.Module):
             pad_amt = x.size(-1) % self.pad_to
             #            if pad_amt != 0:
             x = nn.functional.pad(x, (0, self.pad_to - pad_amt))
-        
-        return x # .to(dtype)
+
+        return x  # .to(dtype)
 
     @classmethod
     def from_config(cls, cfg, log=False):
-        return cls(sample_rate=cfg['sample_rate'], window_size=cfg['window_size'],
+        return cls(sample_rate=cfg['sample_rate'],
+                   window_size=cfg['window_size'],
                    window_stride=cfg['window_stride'], n_fft=cfg['n_fft'],
                    nfilt=cfg['features'], window=cfg['window'],
                    normalize=cfg['normalize'],
                    max_duration=cfg.get('max_duration', 16.7),
                    dither=cfg['dither'], pad_to=cfg.get("pad_to", 0),
                    frame_splicing=cfg.get("frame_splicing", 1), log=log)
+
 
 class FeatureFactory(object):
     featurizers = {
@@ -363,5 +397,5 @@ class FeatureFactory(object):
     def from_config(cls, cfg):
         feat_type = cfg.get('feat_type', "logspect")
         featurizer = cls.featurizers[feat_type]
-        #return featurizer.from_config(cfg, log="log" in cfg['feat_type'])
+        # return featurizer.from_config(cfg, log="log" in cfg['feat_type'])
         return featurizer.from_config(cfg, log="log" in feat_type)
